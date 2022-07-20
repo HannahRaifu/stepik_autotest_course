1354234ebcea79042c0707abfec6c4e2c9c7c07f
Hannah Raifu <umegami@gmail.com>
2022-07-20 21:59:26 +0300

    запуск браузера

---
+++ lesson1_step2_11.py
@@ -0,0 +1,19 @@
+﻿import time 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# инициализируем драйвер браузера.
+driver = webdriver.Chrome()
+time.sleep(20)
+# открываем страницу
+driver.get("https://stepik.org/lesson/25969/step/12") 
+time.sleep(20)
+textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+# передаем в поле ответ
+textarea.send_keys("get()")
+time.sleep(5)
+submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
+submit_button.click() 
+time.sleep(5)
+#закрыть окно браузера
+driver.quit()
